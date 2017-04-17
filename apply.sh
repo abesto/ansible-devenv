433d6c95199d4d4b3d6024a1b8260f159aff8684
Zoltan Nagy <abesto0@gmail.com>
2017-04-17 12:48:22 +0200

    Fix #43, fix #44: install bats, shellcheck

--- apply.sh
+++ apply.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 set +u; . virtualenv/bin/activate; set -u  # remove once lastpass lookup plugin is released
+
+if ! lpass status; then
+    lpass login zoltan.nagy@prezi.com
+fi
+
 ansible-playbook -v site.yml -i inventory --ask-become-pass "$@"
