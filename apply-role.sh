b4d7c46d95469e3c69e28f3c54626288c86dca22
Zoltan Nagy <abesto0@gmail.com>
2017-06-09 08:25:24 +0200

    Split stalonetray config by resolution

--- apply-role.sh
+++ apply-role.sh
@@ -2,16 +2,10 @@
 # Applies a single role. Useful when making small changes, or developing a role.
 
 set -euo pipefail
-set +u; . virtualenv/bin/activate; set -u  # remove once lastpass lookup plugin is released
 
 site="$1"; shift
 role="$1"; shift
 
-if ! lpass status; then
-    lpass login "$(cat "$site".email)"
-fi
-
-trap "rm -v $role.yml" EXIT
 cat > "$role.yml" <<EOF
 - hosts: localhost
   gather_subset:
@@ -19,6 +13,7 @@ cat > "$role.yml" <<EOF
   roles:
     - $role
 EOF
+trap "rm -v $role.yml" EXIT
 
 echo "Applying role $role. Playbook follows."
 echo "******************"
@@ -26,4 +21,7 @@ cat "$role.yml"
 echo "******************"
 echo
 
-ansible-playbook -i inventory --ask-become-pass -v "$role.yml" "$@"
+cp "$site.email" "$role.email"
+trap "rm -v $role.yml $role.email" EXIT
+
+./apply.sh "$role" "$@"
